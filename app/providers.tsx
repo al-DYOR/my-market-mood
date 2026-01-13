1b3e29378f2c9edbb422306f5b217732536a93e7
Al D <93335072+al-DYOR@users.noreply.github.com>
2026-01-14 01:36:39 +0500

    add wagmi providers

---
+++ app/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { WagmiConfig, createConfig, base } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { http } from 'viem';
+
+const config = createConfig({
+  chains: [base],
+  transports: { [base.id]: http() }
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiConfig config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiConfig>
+  );
+}
