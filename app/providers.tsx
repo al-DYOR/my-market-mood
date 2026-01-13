0fc5b337a5159e50200be42068d0d8130af9004b
Al D <93335072+al-DYOR@users.noreply.github.com>
2026-01-14 01:51:21 +0500

    Update wagmi v2

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,21 @@
 'use client';
-import { WagmiConfig, createConfig, base } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
+import { base } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { http } from 'viem';
 
-const config = createConfig({
+const queryClient = new QueryClient();
+const config = {
   chains: [base],
   transports: { [base.id]: http() }
-});
-
-const queryClient = new QueryClient();
+};
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiConfig config={config}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
-    </WagmiConfig>
+    </WagmiProvider>
   );
 }
