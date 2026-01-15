7a9082138505b097d356a06d0b0833e6927d3ed8
Al D <93335072+al-DYOR@users.noreply.github.com>
2026-01-15 20:31:31 +0500

    Create page.tsx

---
+++ app/not-found/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: '100px 20px', textAlign: 'center' }}>
+      <h1>404 - Page not found</h1>
+      <p>Sorry, we couldn't find that page.<a href="/">main</a></p>
+    </div>
+  )
+}
